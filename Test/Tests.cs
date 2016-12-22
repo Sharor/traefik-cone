59bc9fe665f1126e726a1cfbc8634282ac3937ab
Sharor <johannes_christensen@msn.com>
2016-12-22 16:06:32 +0100

    added test class, will run unit tests from there

---
+++ Test/Tests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xunit;
+using TraefikCone; 
+
+namespace Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void IndentCheck() 
+        {
+			IngressGenerator access = new IngressGenerator();
+			string testIndent = access.FixIndention(0, "hello!");
+			string testIndentOne = access.FixIndention(1, "hello!");
+			string testIndentTwo = access.FixIndention(2, "hello!");
+
+			Assert.Equal("hello!", testIndent);
+			Assert.Equal("  hello!", testIndentOne);
+			Assert.Equal("    hello!", testIndentTwo);
+		}
+    }
+}
