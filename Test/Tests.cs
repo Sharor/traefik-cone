3a2a4d28852f2c523c06a6a4efb6242d0ffbf59a
Sharor <johannes_christensen@msn.com>
2016-12-22 16:53:40 +0100

    added passing tests which cover the functionality

--- Test/Tests.cs
+++ Test/Tests.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using Xunit;
-using TraefikCone; 
+﻿using Xunit;
+using TraefikCone;
+using System.Collections.Generic;
 
 namespace Tests
 {
@@ -18,5 +18,38 @@ namespace Tests
 			Assert.Equal("  hello!", testIndentOne);
 			Assert.Equal("    hello!", testIndentTwo);
 		}
+
+		[Fact]
+		public void IngressCreation()
+		{
+			IngressGenerator access = new IngressGenerator();
+			List<string> ingress = access.CreateIngress("test");
+
+			Assert.Equal(ingress[0], "apiVersion: extensions/v1beta1");
+			Assert.Equal(ingress[1], "kind: Ingress");
+			Assert.Equal(ingress[2], "metadata:");
+			Assert.Equal(ingress[3], "  name: test-ing");
+			Assert.Equal(ingress[4], "spec:");
+			Assert.Equal(ingress[5], "  rules:");
+			Assert.Equal(ingress[6], "  - host: test.local");
+			Assert.Equal(ingress[7], "    http:");
+			Assert.Equal(ingress[8], "      paths:");
+			Assert.Equal(ingress[9], "      - path: /");
+			Assert.Equal(ingress[10], "        backend:");
+			Assert.Equal(ingress[11], "          serviceName: test-svc");
+			Assert.Equal(ingress[12], "          servicePort: 80");
+		}
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
--- TraefikCone/IngressGenerator.cs
+++ TraefikCone/IngressGenerator.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace TraefikCone
 {
+	/// <summary>
+	/// This class is responsible for writing out a file that represents an ingress in a træfik cluster, when prompted from a pipeline. 
+	/// </summary>
     public class IngressGenerator
     {
 		/// <summary>
@@ -26,27 +29,41 @@ namespace TraefikCone
 		}
 
 		/// <summary>
+		/// Loops through file structure and writes out each line to a physical file. 
+		/// </summary>
+		/// <param name="fileStructure">List of strings, each list element represents a line in the file.</param>
+		/// <param name="path">The path to save the file.</param>
+		public void WriteOut(List<string> fileStructure, string path)
+		{
+
+			Stream fileStream = File.Create(path);
+			using (StreamWriter file = new StreamWriter(fileStream))
+			{
+				foreach (string line in fileStructure)
+					file.WriteLine(line); 
+			}
+		}
+
+		/// <summary>
 		/// Creates an ingress rule for a given pipeline.
 		/// Choices that have been made implicitly, are that træfik is running, and that the service is tested in 
 		/// context of its correct placement (ie access goes through root on / )
 		/// </summary>
 		/// <param name="path">Path to save file</param>
 		/// <param name="name">Ingress name</param>
-		public void CreateIngress(string path, string name)
+		public List<string> CreateIngress(string name)
 		{
 			#region Conventions for ingress
 			string ingressName = string.Format("{0}-ing", name);
 			string host = string.Format("{0}.local", name);
-			string svcName = string.Format("{0}-svc", name); 
+			string svcName = string.Format("{0}-svc", name);
 			#endregion
 
-			Stream fileStream = File.Create(path);
-			using (StreamWriter file = new StreamWriter(fileStream))
-			{
-				WriteIngressMetadata(file, ingressName);
-				WriteIngressSpec(file, host, svcName); 
+			List<string> ingress = new List<string>();
+			ingress = WriteIngressMetadata(ingress, ingressName);
+			ingress = WriteIngressSpec(ingress, host, svcName);
 
-			}
+			return ingress; 
 		}
 
 		/// <summary>
@@ -56,14 +73,15 @@ namespace TraefikCone
 		/// metadata:
 		///   name: ingress-name
 		/// </summary>
-		/// <param name="file">The file being written to, in this context its an ingress yml.</param>
+		/// <param name="file">The structure of the file to write to, in this context its an ingress yml.</param>
 		/// <param name="ingressName">Name for the ingress in the kubernetes cluster</param>
-		void WriteIngressMetadata(StreamWriter file, string ingressName)
+		List<string> WriteIngressMetadata(List<string> file, string ingressName)
 		{
-			file.WriteLine("apiVersion: extensions / v1beta1");
-			file.WriteLine("kind: Ingress");
-			file.WriteLine("metadata:");
-			file.WriteLine(FixIndention(1, string.Format("name: {0}", ingressName)));
+			file.Add("apiVersion: extensions/v1beta1");
+			file.Add("kind: Ingress");
+			file.Add("metadata:");
+			file.Add(FixIndention(1, string.Format("name: {0}", ingressName)));
+			return file; 
 		}
 
 		/// <summary>
@@ -81,17 +99,18 @@ namespace TraefikCone
 		/// <param name="file"></param>
 		/// <param name="hostName"></param>
 		/// <param name="serviceName"></param>
-		void WriteIngressSpec(StreamWriter file, string hostName, string serviceName)
+		List<string> WriteIngressSpec(List<string> file, string hostName, string serviceName)
 		{
-			file.WriteLine("spec:");
-			file.WriteLine(FixIndention(1, "rules:"));
-			file.WriteLine(FixIndention(1, string.Format("- host: {0}", hostName)));
-			file.WriteLine(FixIndention(2, "http:"));
-			file.WriteLine(FixIndention(3, "paths:"));
-			file.WriteLine(FixIndention(3, "- path: /"));
-			file.WriteLine(FixIndention(4, "backend:"));
-			file.WriteLine(FixIndention(5, string.Format("serviceName: {0}", serviceName)));
-			file.WriteLine(FixIndention(5, "servicePort: 80"));
+			file.Add("spec:");
+			file.Add(FixIndention(1, "rules:"));
+			file.Add(FixIndention(1, string.Format("- host: {0}", hostName)));
+			file.Add(FixIndention(2, "http:"));
+			file.Add(FixIndention(3, "paths:"));
+			file.Add(FixIndention(3, "- path: /"));
+			file.Add(FixIndention(4, "backend:"));
+			file.Add(FixIndention(5, string.Format("serviceName: {0}", serviceName)));
+			file.Add(FixIndention(5, "servicePort: 80"));
+			return file; 
 		}
 	}
 }
